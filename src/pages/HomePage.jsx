d3c652e319fa01ea4efe7a46a98718f4fcfe4da6
eduard-lab-front-end <eduard-lab@web.de>
2024-09-19 12:38:19 +0200

    Done

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,6 @@ import studentsData from "../assets/students.json";
 function HomePage() {
   // eslint-disable-next-line no-unused-vars
   const [students, setStudents] = useState(studentsData);
-
   return (
     <div className="border-2 border-rose-500 m-2">
         <h1>Home Page</h1>
